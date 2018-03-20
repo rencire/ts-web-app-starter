07e4c79d48923138c1388b10f97d46f90a708f7c
Eric Ren <546296+rencire@users.noreply.github.com>
2018-03-19 21:56:01 -0700

    modified husky rc

--- husky.config.js
+++ husky.config.js
@@ -3,6 +3,6 @@ module.exports = {
     "pre-commit": "lint-staged",
     //"prepare-commit-msg": "./gitHooks/prepare-commit-msg ${GIT_PARAMS}",
     // "commit-msg": "./gitHooks/commit-msg ${GIT_PARAMS}",
-    "pre-push": "jest -c .jestrc.json --bail"
+    "pre-push": "jest --bail"
   }
 };
