c7a9f99ebb0963a24c8211c258047b4667ff4a8b
Eric Ren <546296+rencire@users.noreply.github.com>
2018-03-19 19:07:52 -0700

    Changed husky config file

--- husky.config.js
+++ husky.config.js
@@ -1,8 +1,8 @@
-{
-  "hooks": {
+module.exports = {
+  hooks: {
     "pre-commit": "lint-staged",
     //"prepare-commit-msg": "./gitHooks/prepare-commit-msg ${GIT_PARAMS}",
     // "commit-msg": "./gitHooks/commit-msg ${GIT_PARAMS}",
     "pre-push": "jest -c .jestrc.json --bail"
   }
-}
+};
--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,3 +1,4 @@
+// Added declaration to allow importing css files via `css modules`
 // https://stackoverflow.com/questions/40382842/cant-import-css-scss-modules-typescript-says-cannot-find-module#41946697
 declare module "*.css" {
   const content: { [className: string]: string };
